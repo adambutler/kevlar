17d38c8846cd843212c6fa212f73683ffd28c358
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-07 23:15:30 +0100

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.force_ssl = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
